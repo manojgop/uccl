423e7bb6a2f6ddd3cd1003c7d44a5b28c579a9e7
Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>
2025-11-05 06:22:53 -0500

    Disable unsupported CQ attributes for Intel MKP NIC
    Intel MKP NIC doesn't support certain CQ Ex attributes like
    IBV_CREATE_CQ_ATTR_IGNORE_OVERRUN and IBV_CQ_ATTR_MODERATE. UCCL throws
    run time error if we set these attributes.
    
    Signed-off-by: Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>

--- collective/rdma/util_rdma.h
+++ collective/rdma/util_rdma.h
@@ -83,8 +83,9 @@ static inline void util_rdma_create_qp_seperate_cq(
       if constexpr (kTestNoHWTimestamp)
         cq_ex_attr.wc_flags &= ~IBV_WC_EX_WITH_COMPLETION_TIMESTAMP;
       cq_ex_attr.comp_mask = IBV_CQ_INIT_ATTR_MASK_FLAGS;
-      cq_ex_attr.flags = IBV_CREATE_CQ_ATTR_SINGLE_THREADED |
-                         IBV_CREATE_CQ_ATTR_IGNORE_OVERRUN;
+      cq_ex_attr.flags = IBV_CREATE_CQ_ATTR_SINGLE_THREADED;
+      //cq_ex_attr.flags = IBV_CREATE_CQ_ATTR_SINGLE_THREADED |
+      //                   IBV_CREATE_CQ_ATTR_IGNORE_OVERRUN;
       auto scq_ex = (struct ibv_cq_ex**)scq;
       *scq_ex = ibv_create_cq_ex(context, &cq_ex_attr);
       UCCL_INIT_CHECK(*scq_ex != nullptr, "ibv_create_cq_ex failed");
@@ -156,8 +157,9 @@ static inline void util_rdma_create_qp(
       if constexpr (kTestNoHWTimestamp)
         cq_ex_attr.wc_flags &= ~IBV_WC_EX_WITH_COMPLETION_TIMESTAMP;
       cq_ex_attr.comp_mask = IBV_CQ_INIT_ATTR_MASK_FLAGS;
-      cq_ex_attr.flags = IBV_CREATE_CQ_ATTR_SINGLE_THREADED |
-                         IBV_CREATE_CQ_ATTR_IGNORE_OVERRUN;
+      cq_ex_attr.flags = IBV_CREATE_CQ_ATTR_SINGLE_THREADED;
+      //cq_ex_attr.flags = IBV_CREATE_CQ_ATTR_SINGLE_THREADED |
+      //                   IBV_CREATE_CQ_ATTR_IGNORE_OVERRUN;
       auto cq_ex = (struct ibv_cq_ex**)cq;
       *cq_ex = ibv_create_cq_ex(context, &cq_ex_attr);
       UCCL_INIT_CHECK(*cq_ex != nullptr, "ibv_create_cq_ex failed");
@@ -293,8 +295,8 @@ static inline struct ibv_cq_ex* util_rdma_create_cq_ex(
       IBV_WC_EX_WITH_SRC_QP |
       IBV_WC_EX_WITH_COMPLETION_TIMESTAMP;  // Timestamp support.
   cq_ex_attr.comp_mask = IBV_CQ_INIT_ATTR_MASK_FLAGS;
-  cq_ex_attr.flags =
-      IBV_CREATE_CQ_ATTR_SINGLE_THREADED | IBV_CREATE_CQ_ATTR_IGNORE_OVERRUN;
+  cq_ex_attr.flags = IBV_CREATE_CQ_ATTR_SINGLE_THREADED;
+      //IBV_CREATE_CQ_ATTR_SINGLE_THREADED | IBV_CREATE_CQ_ATTR_IGNORE_OVERRUN;
 
   if constexpr (kTestNoHWTimestamp)
     cq_ex_attr.wc_flags &= ~IBV_WC_EX_WITH_COMPLETION_TIMESTAMP;
@@ -311,7 +313,8 @@ static inline int util_rdma_modify_cq_attr(struct ibv_cq_ex* cq_ex,
   cq_attr.moderate.cq_count = cq_count;
   cq_attr.moderate.cq_period = cq_period;
 
-  return ibv_modify_cq(ibv_cq_ex_to_cq(cq_ex), &cq_attr);
+  //return ibv_modify_cq(ibv_cq_ex_to_cq(cq_ex), &cq_attr);
+  return 0;
 }
 
 /**
