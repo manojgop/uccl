e02ebd9e10b15f96dff3272eb06cf937c5ebbd33
Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>
2025-12-08 07:39:55 -0500

    NCCL test script to test UD with Intel RDMA NIC
    Signed-off-by: Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>

---
+++ collective/efa/set_gid_index.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export UCCL_ENA_DEVICES="eno0"
+export UCCL_EFA_DEVICES="irdma-mkp0"
+if [ "$(hostname)" = "emrh01" ]; then
+  export NCCL_IB_GID_INDEX=2
+elif [ "$(hostname)" = "emrh02" ]; then
+  export NCCL_IB_GID_INDEX=2
+elif [ "$(hostname)" = "emrh03" ]; then
+  export NCCL_IB_GID_INDEX=1
+elif [ "$(hostname)" = "emrh04" ]; then
+  export NCCL_IB_GID_INDEX=1
+fi
+exec "$@"
+
