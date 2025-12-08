bacbd5c34354ad51b47e85f21cce6082b4d5f58b
Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>
2025-12-08 07:08:15 -0500

    Changes to support UD with Intel RDMA NIC in collective EFA
    - Support irdma NIC and vendor id 8086
    - Add Ethernet support in link layer
    - Use cudaMallocaManaged() since MEV doesn't support GPU Direct RDMA
    - Change number of GPU devices to 1
    - Chnage hardcoding of GID INDEX to 2 for now
    - Change MTU to 4096
    
    Signed-off-by: Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>

--- collective/efa/util_efa.cc
+++ collective/efa/util_efa.cc
@@ -44,13 +44,16 @@ static void init_device_name_lists() {
         char const* name = ibv_get_device_name(list[i]);
         bool ok = false;
         if (name &&
-            (strncmp(name, "rdmap", 5) == 0 || strncmp(name, "efa", 3) == 0)) {
+	    (strncmp(name, "rdmap", 5) == 0 || strncmp(name, "efa", 3) == 0 ||
+             strncmp(name, "irdma", 5) == 0)) {
           ok = true;
         }
         struct ibv_context* ctx = ibv_open_device(list[i]);
         if (ctx) {
           struct ibv_device_attr attr;
-          if (ibv_query_device(ctx, &attr) == 0 && attr.vendor_id == 0x1d0f) {
+	  // Accept AWS EFA (0x1d0f) and Intel RDMA (0x8086)
+          if (ibv_query_device(ctx, &attr) == 0 &&
+              (attr.vendor_id == 0x1d0f || attr.vendor_id == 0x8086)) {
             ok = true;
           }
           ibv_close_device(ctx);
@@ -184,8 +187,11 @@ void EFAFactory::InitDev(int dev_idx) {
     goto close_device;
   }
 
-  if (port_attr.link_layer != IBV_LINK_LAYER_UNSPECIFIED) {
-    fprintf(stderr, "EFA link layer is not supported\n");
+  // Accept both EFA (UNSPECIFIED) and RDMA (ETHERNET/RoCE) link layers
+  if (port_attr.link_layer != IBV_LINK_LAYER_UNSPECIFIED &&
+      port_attr.link_layer != IBV_LINK_LAYER_ETHERNET) {
+    fprintf(stderr, "Link layer %d not supported (expected UNSPECIFIED or ETHERNET)\n",
+            port_attr.link_layer);
     goto close_device;
   }
 
@@ -340,17 +346,34 @@ EFASocket::EFASocket(int gpu_idx, int dev_idx, int socket_idx)
 
   // Allocate memory for packet data.
   void* pkt_data_buf_ = nullptr;
+#ifdef INTEL_RDMA_NIC
+  // Intel MEV NIC does not support GPUDirect. So ibv_reg_mr will fail with cudaMalloc
+  // cudaMallocManaged allocates a unified single virtual address range that is accessible
+  // from both CPU and GPU. Runtime migrates pages on demand between host and device when
+  // either side accesses them, using page faults to trigger migration.
+  auto cuda_ret = cudaMallocManaged(&pkt_data_buf_, PktDataBuffPool::kNumPktData *
+                                               PktDataBuffPool::kPktDataSize,cudaMemAttachGlobal);
+  DCHECK(cuda_ret == cudaSuccess) << "cudaMallocManaged failed";
+#else
   auto cuda_ret = cudaMalloc(&pkt_data_buf_, PktDataBuffPool::kNumPktData *
                                                  PktDataBuffPool::kPktDataSize);
   DCHECK(cuda_ret == cudaSuccess) << "cudaMalloc failed";
+#endif
   LOG(INFO) << "[EFA] gpu_idx " << gpu_idx << " pkt_data_buf_ " << std::hex
             << pkt_data_buf_ << " to "
             << pkt_data_buf_ +
                    PktDataBuffPool::kNumPktData * PktDataBuffPool::kPktDataSize;
+#ifdef INTEL_RDMA_NIC
+  auto* pkt_data_mr_ =
+      ibv_reg_mr(pd_, pkt_data_buf_,
+                 PktDataBuffPool::kNumPktData * PktDataBuffPool::kPktDataSize,
+                 IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE |IBV_ACCESS_REMOTE_READ);
+#else
   auto* pkt_data_mr_ =
       ibv_reg_mr(pd_, pkt_data_buf_,
                  PktDataBuffPool::kNumPktData * PktDataBuffPool::kPktDataSize,
                  IBV_ACCESS_LOCAL_WRITE);
+#endif
   DCHECK(pkt_data_mr_ != nullptr) << "ibv_reg_mr failed";
   pkt_data_pool_ = new PktDataBuffPool(pkt_data_mr_);
 
