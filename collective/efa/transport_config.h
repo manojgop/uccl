5fe5c02c0eb580264d2ade2a119147e67afd371e
Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>
2026-01-14 00:35:46 -0500

    Enable EMULATE_RC_ZC and disable SCATTERED_MEMCPY
    Signed-off-by: Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>

--- collective/efa/transport_config.h
+++ collective/efa/transport_config.h
@@ -7,8 +7,8 @@
 #define REXMIT_SET_PATH
 // #define USE_SRD
 // #define USE_SRD_FOR_CTRL
-// #define EMULATE_RC_ZC
-#define SCATTERED_MEMCPY
+#define EMULATE_RC_ZC
+// #define SCATTERED_MEMCPY
 // #define RTT_STATS
 #define LAZY_CREATE_ENGINE
 
